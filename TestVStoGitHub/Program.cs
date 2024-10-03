17e0aa3514212d7b775159e4be8927b6eced562e
Anita Wood <arwood2@dmacc.edu>
2024-10-03 08:58:30 -0500

    Add project files.

---
+++ TestVStoGitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestVStoGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
